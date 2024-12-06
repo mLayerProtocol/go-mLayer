19678351079a838974656edee0c87ace7ac9015d
Femi Olowonefa <feminefa@gmail.com>
2024-12-06 00:21:57 -0500

    fixed topic event counter bug

--- entities/message.go
+++ entities/message.go
@@ -274,7 +274,7 @@ func (msg Message) EncodeBytes() ([]byte, error) {
 	}
 
 	dataByte, _ := hex.DecodeString(msg.Data)
-	logger.Debugf("DataBytes: %s %s %s %s", dataByte, msg.DataType, msg.Receiver, hex.EncodeToString(utils.UuidToBytes(msg.Topic)))
+	// logger.Debugf("DataBytes: %s %s %s %s", dataByte, msg.DataType, msg.Receiver, hex.EncodeToString(utils.UuidToBytes(msg.Topic)))
 	return encoder.EncodeBytes(
 		encoder.EncoderParam{Type: encoder.ByteEncoderDataType, Value: actions},
 		// encoder.EncoderParam{Type: encoder.ByteEncoderDataType, Value: attachments},
